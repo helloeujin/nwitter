edb424148eebaffb0e0ffb2bf7735f239b1032f1
Youjin <meggonagul@gmail.com>
2022-12-12 21:46:20 +0900

    firebase imported

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7oXFl6m2wCGp5GIidmh14b5rg6npajZA",
+  authDomain: "nwitter-ddb02.firebaseapp.com",
+  projectId: "nwitter-ddb02",
+  storageBucket: "nwitter-ddb02.appspot.com",
+  messagingSenderId: "203677732971",
+  appId: "1:203677732971:web:620aac110a95b0650d612e",
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
