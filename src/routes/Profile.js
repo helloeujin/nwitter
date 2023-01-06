b4551ae5890de3c2af7a389aeb00337ba1bd9c27
Youjin <meggonagul@gmail.com>
2023-01-06 23:09:45 +0900

    sign out, redirect, react navigation and route

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,16 @@
 import React from "react";
+import { signOut } from "firebase/auth";
+import authService from "fbase";
+import { useHistory } from "react-router-dom";
 
-export default () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    signOut(authService);
+    history.push("/");
+  };
+
+  return <button onClick={onLogOutClick}>Log Out</button>;
+};
+
+export default Profile;
